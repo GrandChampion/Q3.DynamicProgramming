b35e7a60d7bd881b74883bda7f823be1df71223e
Yeongu Choe <yeongu@Yeongus-MacBook-Air.local>
2023-03-07 05:11:22 -0800

    1

--- 1D problem/Coin Change.py
+++ 1D problem/Coin Change.py
@@ -19,48 +19,49 @@ def MemoCoinChangeHelper(sol, n):
             sol, n-25)+1, MemoCoinChangeHelper(sol, n-10)+1, MemoCoinChangeHelper(sol, n-1)+1)
     return sol[n]
 
-# Dynamic Coin Change (Bottom->Up: using for loop)
+# # Dynamic Coin Change (Bottom->Up: using for loop)
 
 
-def dynamicCoinChange(n):
-    solution = [-1 for i in range(n+1)]
-    for i in range(1, n+1):
-        solution[i] = min(dynamicCoinChangeHelper(solution, i-25)+1, dynamicCoinChangeHelper(
-            solution, i-10)+1, dynamicCoinChangeHelper(solution, i-1)+1)
-    return solution[n]
+# def dynamicCoinChange(n):
+#     solution = [-1 for i in range(n+1)]
+#     for i in range(1, n+1):
+#         solution[i] = min(dynamicCoinChangeHelper(solution, i-25)+1, dynamicCoinChangeHelper(
+#             solution, i-10)+1, dynamicCoinChangeHelper(solution, i-1)+1)
+#     return solution[n]
 
-# API
+# # API
 
-def dynamicCoinChangeHelper(sol, k):
-    if k < 0:
-        return np.inf
-    elif k == 0:
-        return 0
-    else:
-        return sol[k]
+# def dynamicCoinChangeHelper(sol, k):
+#     if k < 0:
+#         return np.inf
+#     elif k == 0:
+#         return 0
+#     else:
+#         return sol[k]
 
-print(dynamicCoinChange(100))
+# print(dynamicCoinChange(100))
 
-# # Extract coin types
-# solution1 = [-1 for i in range(10)]
-# MemoCoinChangeHelper(solution1, 9)
-# print(solution1)
+# Extract coin types
+solution1 = [-1 for i in range(28)]
+MemoCoinChangeHelper(solution1, 27)
+print(solution1)
 
 
-# def extractChange(solution, n):
-#     twentyFive = 0
-#     ten = 0
-#     one = 0
-#     while n > 0:
-#         if solution[n] == solution[n-1]+1 or n == 1:
-#             n -= 1
-#             one += 1
-#         elif solution[n] == solution[n-10]+1:
-#             n -= 1
-#             ten += 1
-#         else:
-#             n -= 1
-#             twentyFive += 1
-#     return (one, ten, twentyFive)
+def extractChange(solution, n):
+    twentyFive = 0
+    ten = 0
+    one = 0
+    while n > 0:
+        if solution[n] == solution[n-1]+1 or n == 1:
+            n -= 1
+            one += 1
+        elif solution[n] == solution[n-10]+1:
+            n -= 10
+            ten += 1
+        else:
+            n -= 25
+            twentyFive += 1
+    return (one, ten, twentyFive)
 
-# print(extractChange(solution1, 9))
+
+print(extractChange(solution1, 27))
